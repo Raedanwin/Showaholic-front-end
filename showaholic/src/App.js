788400310a8204de061fbcaacd5e2bfc2b060b1a
Raedanwin <Raedanwin@gmail.com>
2020-12-17 23:16:29 -0500

    search done, probably last commit of any substance until after presentation

--- showaholic/src/App.js
+++ showaholic/src/App.js
@@ -19,23 +19,28 @@ import ShowShow from './components/show_crud/Show.jsx'
 import Shows from './components/Shows.jsx'
 import ShowCreate from './components/show_crud/Create'
 import ShowEdit from './components/show_crud/Edit'
+import Search from './components/Search'
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path={'/watchlist/:id/show/create'} component={ShowCreate} />
-          <Route exact path={'/watchlist/:id/show/:showId/edit'} component={ShowEdit} />
-          <Route exact path={'/watchlist/:id/show/:showId'} component={ShowShow} />
-          <Route exact path={'/watchlist/:id/show/'} component={Shows} />
-          <Route exact path={'/watchlist/create'} component={WatchCreate} />
-          <Route exact path={'/watchlist/:id/edit'} component={WatchEdit} />
-          <Route exact path={'/watchlist/:id'} component={WatchShow} />
-          <Route exact path={'/collections'} component={Collections} />
-          <Route exact path={'/'} exact component={Home} />
-        </Switch>
-      </Router>
+      <div>
+        <h1></h1>
+      </div>
+      // <Router>
+      //   <Switch>
+      //     <Route exact path={'/watchlist/:id/show/create'} component={ShowCreate} />
+      //     <Route exact path={'/watchlist/:id/show/:showId/edit'} component={ShowEdit} />
+      //     <Route exact path={'/watchlist/:id/show/:showId'} component={ShowShow} />
+      //     <Route exact path={'/watchlist/:id/show/'} component={Shows} />
+      //     <Route exact path={'/watchlist/create'} component={WatchCreate} />
+      //     <Route exact path={'/watchlist/:id/edit'} component={WatchEdit} />
+      //     <Route exact path={'/watchlist/:id'} component={WatchShow} />
+      //     <Route exact path={'/collections'} component={Collections} />
+      //     <Route exact path={'/search'} component={Search}/>
+      //     <Route exact path={'/'} exact component={Home} />
+      //   </Switch>
+      // </Router>
     )
   }
 }
--- showaholic/src/components/Search.jsx
+++ showaholic/src/components/Search.jsx
@@ -1,17 +1,61 @@
 import React, { Component } from 'react'
+import axios from "axios";
+
+
+
 
 export default class Search extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            allResults: [],
+            search: " "
         }
+        this.getSearch = this.getSearch.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
+    componentDidMount() {
+    
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+    getSearch = () => {
+        const searchQuestion = this.state.search
+        console.log("TEST")
+        const options = {
+            method: 'GET',
+            url: 'https://ott-details.p.rapidapi.com/search',
+            params: { title: searchQuestion, page: '1' },
+            headers: {
+                'x-rapidapi-key': 'd7807b4709mshe1476f2340682bbp1393dajsn3cbd1de705d0',
+                'x-rapidapi-host': 'ott-details.p.rapidapi.com'
+            }
+        };
+        axios.request(options).then(function (response) {
+            console.log(response.data.results)
+            return response.data.results
+        }).then(res => this.setState({ allResults: res })).catch(function (error) {
+            console.error(error);
+        });
 
+
+    }
     render() {
-        return(
+        return (
             <div>
-                
+ 
+                <input onChange={this.handleChange} id='search' type='text' />
+                <button onClick={this.getSearch}>Search</button>
+                {this.state.allResults.map(result => {
+                    return (
+                        <div>
+                            {result.title}
+                        </div>
+                    )
+                })}
             </div>
         )
     }
